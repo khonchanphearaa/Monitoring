a821a69a878e0a6ffe74b9e2496cf5080f59339a
khonchanphearaa007 <khonchanphearaa@gmail.com>
2026-08-04 08:28:16 +0700

    config

--- config.py
+++ config.py
@@ -7,15 +7,15 @@ class Config:
     OPENCLAW_WEBHOOK_URL = os.getenv("OPENCLAW_WEBHOOK_URL")
     SMART_LIGHT_API_URL = os.getenv("SMART_LIGHT_API_URL")
     OPENCLAW_HOOK_TOKEN = os.getenv("OPENCLAW_HOOK_TOKEN")
-    MAX_DEPLOY_MINUTES = int(os.getenv("MAX_DEPLOY_MINUTES", 15))
+    MAX_DEPLOY_MINUTES = os.getenv("MAX_DEPLOY_MINUTES")
 
     # Check if missing
     @staticmethod
     def validate():
         if not Config.OPENCLAW_WEBHOOK_URL:
-            raise ValueError("Missing OPENCLAW_WEBHOOK_URL")
+            raise ValueError("Missing OPENCLAW_WEBHOOK_URL env")
         if not Config.SMART_LIGHT_API_URL:
-            raise ValueError("Missing SMART_LIGHT_API_URL")
+            raise ValueError("Missing SMART_LIGHT_API_URL env")
         if not Config.OPENCLAW_HOOK_TOKEN:
-            raise ValueError("Missing OPENCLAW_HOOK_TOKEN")
-        
+            raise ValueError("Missing OPENCLAW_HOOK_TOKEN env")
+
